f01fa9962cf5b786dc449de7550f7a37484af79c
Anastasiya-Vavilova <ntttru@bk.ru>
2024-08-12 20:56:35 +0300

    Initial commit

---
+++ src/main/java/ru/netology/service/Main.java
@@ -0,0 +1,9 @@
+package ru.netology.service;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CashbackHackService service = new CashbackHackService();
+        System.out.println("Для получения максимального количество кэшбэка, Вам необходимо докупить на " + service.remain(1111) + " руб.");
+    }
+}
