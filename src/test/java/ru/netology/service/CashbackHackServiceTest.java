edd8834b470907e7669d88200d2191719663b125
Anastasiya-Vavilova <ntttru@bk.ru>
2024-08-13 11:31:43 +0300

    dependencies change

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -3,10 +3,10 @@ package ru.netology.service;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
-
 
     CashbackHackService service = new CashbackHackService();
 
@@ -15,7 +15,5 @@ public class CashbackHackServiceTest {
         int actual = service.remain(1000);
         int expected = 0;
         Assert.assertEquals(actual, expected);
-
-
     }
 }
